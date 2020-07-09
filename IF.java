1deaa0b36c6444ebdd9ca0d7cd3044b5da9a7162
Kurt <52134197+KurtHilder@users.noreply.github.com>
2020-07-09 12:28:34 +0200

    9/7 - Finished IF
    Done

--- IF.java
+++ IF.java
@@ -9,7 +9,7 @@ public class IF{
       // you need to decare variables using  
       // their data type.
       int valueA = 5;
-      float valueB = 4.75f;
+      float valueB = 4.7f;
       // to convert to int use method below.
       int valueB_int = (int)valueB;
       System.out.println(valueB_int);
@@ -18,9 +18,28 @@ public class IF{
       if (valueA == 5){
          System.out.println("It's 5");
       }
-      if (valueA != 5){
-         System.out.println("It's not 5");
+      if (valueA != 4){
+         System.out.println("It's not 4");
       }
+      if (valueA < 6){
+         System.out.println("Its less than 6");
+      }
+      if (valueA >= 3){
+         System.out.println("Its equal to or greater than 3");
+      } 
+      // to test two conditions we use the && and ||
+      // these are called logical operators.
+      // the && checks if both conditions are true.
+      // the || checks if one of the conditions are true.
+      if (valueA == 5 && valueB == 4.75f){
+         System.out.println("Both are true");
+      }
+      if (valueA == 5 || valueB == 4.75f){
+         System.out.println("One of them are true");
+      }
+      
+      
+      
       
    }
 }
