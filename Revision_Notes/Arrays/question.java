615b9cb4f7c2fd0181aa65e2fbea02dc54c51cf1
Kurt <52134197+KurtHilder@users.noreply.github.com>
2020-07-28 10:40:16 +0200

    2020-07-28
    Question uploaded

---
+++ Revision_Notes/Arrays/question.java
@@ -0,0 +1,11 @@
+/*
+*  Make an array with 500 elements.
+* The elements in the array must be the first
+* 500 multiples of 3.
+* 
+*  Then print every 5th value in the array.
+*  Number the outputs.
+*  1  3
+*  2  18
+*  3  33
+*/
