ac5b02d5c82852fb5a0505c221481fa9309e7b58
Kurt <52134197+KurtHilder@users.noreply.github.com>
2020-07-29 11:56:42 +0200

    2020-07-29
    This is the answer to the questions...

---
+++ Revision_Notes/Arrays/ArrayTask_2.java
@@ -0,0 +1,21 @@
+public class ArrayTask_2{
+   public static void main(String[] args){
+      
+      int[] threeArray = new int[500];
+      int threes = 0;
+      // for loop to fill value
+      for(int x = 0; x < threeArray.length; x++){
+         threes = threes + 3;
+         threeArray[x] = threes; // fill array  
+         
+      }
+      // for loop to print
+      int counter= 0;
+      for(int y = 0; y < threeArray.length; y++){
+         if(y%5 == 0){
+            counter++;
+            System.out.println(counter + " " + threeArray[y]);
+         }
+      }
+   }
+}
