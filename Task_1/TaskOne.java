60906e8b193a604a927383391b444d36fe2f8989
Kurt <52134197+KurtHilder@users.noreply.github.com>
2020-08-05 10:36:08 +0200

    2020-08-5
    Included Task 1

---
+++ Task_1/TaskOne.java
@@ -0,0 +1,25 @@
+/* Use the following format to
+* print 100 dots (.)
+* use a for loop and if statements to
+* print 10 dots on each line, and then 10 lines.
+* eg. 
+* ..........
+* .......... etc 
+*/
+public class TaskOne{
+   public static void main(String[] args){
+      System.out.println("START");
+      int counter = 0;
+      // WRITE YOUR CODE UNDER THIS COMMENT
+      for(int x = 1; x <= 100;x++){
+         System.out.print(".");
+         // counter++;
+         if(x%10==0){
+            System.out.print("\n");
+         }
+      }
+      
+      // WRITE YOUR CODE ABOVE THIS COMMENT
+      System.out.println("END");
+   }
+}
---
+++ Task_1/TaskTwo.java
@@ -0,0 +1,16 @@
+// use the template to answer the question:
+// 1) If the number is greater than or equal to 3
+//    print "Three"
+// 2) If the number is equal to 2 print "Two"
+// 3) If the number is less than or equal to one
+//    print "One"
+import java.util.Random;
+public class TaskTwo{
+   public static void main(String[] args){
+      Random rand = new Random();
+      int number = rand.nextInt((4-0+1));
+      System.out.println("Random: " + number);
+      // WRITE YOUR CODE UNDER THIS COMMENT
+      // WRITE YOUR CODE ABOVE THIS COMMENT
+   }
+}
